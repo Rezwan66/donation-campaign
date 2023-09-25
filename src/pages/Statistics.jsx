810a1ba2c0348cd67716ce09bb19759a0359129b
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-25 21:17:04 +0600

    added the statistics page pie chart template for chart implementation

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,7 +1,73 @@
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+} from 'recharts';
+
 const Statistics = () => {
+  const data = [
+    { name: 'Total Donation', value: 100 },
+    { name: 'Your Donation', value: 20 },
+  ];
+
+  const COLORS = ['#FF444A', '#00C49F'];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(1)}%`}
+      </text>
+    );
+  };
   return (
-    <div>
-      <h2>Pie chart here statistics page</h2>
+    <div className="border-2 border-red-500">
+      <h2 className="text-center">Pie chart here statistics page</h2>
+
+      <div className="border-2 border-blue-600 flex justify-center">
+        <PieChart width={500} height={500}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={200}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Legend width={500} iconSize={100} iconType="plainline"></Legend>
+          <Tooltip></Tooltip>
+        </PieChart>
+      </div>
     </div>
   );
 };
