556edddb091b1933dd747b810b79bb5dfa343723
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-25 22:30:00 +0600

    implemented handle donate button click to add donations to the local storage

--- src/components/Campaigns/DetailsCard.jsx
+++ src/components/Campaigns/DetailsCard.jsx
@@ -1,7 +1,35 @@
 import PropTypes from 'prop-types';
+import swal from 'sweetalert';
 
 const DetailsCard = ({ campaign }) => {
   const { id, image, title, description, text_color, price } = campaign;
+
+  const handleAddToDonated = () => {
+    const donatedArray = [];
+    const storedDonations = JSON.parse(localStorage.getItem('donations'));
+    if (!storedDonations) {
+      donatedArray.push(campaign);
+      localStorage.setItem('donations', JSON.stringify(donatedArray));
+      swal(
+        'Awesome!',
+        `You have successfully donated $${price} to this campaign`,
+        'success'
+      );
+    } else {
+      const exists = storedDonations.find(campaign => campaign.id === id);
+      if (!exists) {
+        donatedArray.push(...storedDonations, campaign);
+        localStorage.setItem('donations', JSON.stringify(donatedArray));
+        swal(
+          'Awesome!',
+          `You have successfully donated $${price} to this campaign`,
+          'success'
+        );
+      } else {
+        swal('Error!', 'You have already donated to this campaign !', 'error');
+      }
+    }
+  };
   return (
     <div>
       <div className="md:relative">
@@ -14,6 +42,7 @@ const DetailsCard = ({ campaign }) => {
         </div>
         <div className="md:p-7 md:absolute md:bg-[#0B0B0B80] md:bg-opacity-60 md:bottom-0 w-full rounded-b-lg">
           <button
+            onClick={handleAddToDonated}
             className="btn w-full md:w-auto mt-4 md:mt-0 capitalize px-7 py-4 rounded-[4px] text-white border-0"
             style={{ backgroundColor: text_color }}
           >
